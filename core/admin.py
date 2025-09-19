2920e118972dedd613b42cf6feb6bbb9ba6099b9
Moeein <moeeeinaali@gmail.com>
2025-09-19 13:57:03 +0330

    [FIX] make some fields nullable

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Team, TeamMember
 
-# Register your models here.
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'members_count',)
+    search_fields = ('name', 'description')
+    search_fields = ('name',)
+
+    def members_count(self, obj):
+        return obj.members.count()
+
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'team', 'position',)
+    list_filter = ('team',)
+    search_fields = ('first_name', 'last_name', 'email', 'position')
+    ordering = ('-created_at',)
