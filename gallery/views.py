fd50f6e81b72f8c9ff64eb3761dcb16fa6aea871
Moeein Aali <moeeeinaali@gmail.com>
2025-09-29 22:33:20 +0330

    [FEAT] add GalleryImage serializer and view

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .models import GalleryImage
+from papers.views import CustomPagination
 # Create your views here.
+class GalleryImageListView(generics.ListAPIView):
+    queryset = GalleryImage.objects.all()
+    serializer_class = GalleryImageListSerializer
+    permission_classes = [AllowAny]
+    pagination_class = CustomPagination
